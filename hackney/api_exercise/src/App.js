5efdb0f24306b225f5acc8d4585b5f3d1c303c34
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-10 12:54:44 +0200

    working GET request to API edpoint

--- hackney/api_exercise/src/App.js
+++ hackney/api_exercise/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: "https://6kb2p9kgb0.execute-api.eu-west-2.amazonaws.com/",
+  headers: {"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExOCIsImNvbnN1bWVyTmFtZSI6IkludGVydmlld3MiLCJjb25zdW1lclR5cGUiOiIxIiwibmJmIjoxNjIwMzk5NDM3LCJleHAiOjE5MzU5MzIyMzcsImlhdCI6MTYyMDM5OTQzN30.m5Kfn9sQToLnF8mh57GLQYrJnrCQyC0reRuOgT9xv0M"}
+})
+
+
+class App extends Component {
+
+  state = {
+    postcode: "",
+    hasError: false,
+    errorMessage: ""
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleApiError = (errorObject) => {
+    this.setState({hasError: true})
+    this.setState({errorMessage: errorObject})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let givenPostcode = this.state.postcode;
+    api.get(`production/api/v1/addresses/?postcode=${givenPostcode}`)
+    .catch((error) => {this.handleApiError(error.response.data)})
+    // let regex = [A-Za-z][1-9][1-9][A-Za-z]{2};
+    // if (){
+    //   //If the postcode is valid, define the GET request
+    //   // [A-Za-z][1-9] [1-9][A-Za-z]{2}
+    // } else {
+    //   //Otherwise, error handling
+    // }
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit = {this.handleSubmit}>
+          <input name="postcode" type="text" placeholder="Enter Postcode" value={this.state.postcode} onChange={this.handleInput}/>
+          <input type='submit'/>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+//Plan:
+//Create a form that takes in the parameter of a postcode
+//onSubmit the postcode from the form will be sent in a GET request to the given API endpoint
+//Validate postcode with given Regex 
+//If correct, process with HTTP request
+//If incorrect, provide error handling
+//Render the addresses for that postcode from the HTTP response 
